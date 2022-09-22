7632c1d8ef01b87850cce28de0d92fffc2eb049b
Jimmy CHAVES <jimmybakerchaves@gmail.com>
2022-09-22 12:03:35 +0200

    Basic exercises done

--- TM Jimmy/Basic/basic.js
+++ TM Jimmy/Basic/basic.js
@@ -29,3 +29,42 @@ function crystalGlazer(NumberOfChildren, partnerName, location, jobTitle) {
 console.log(crystalGlazer(2, "Zuzana", "Wien", "web developer" ));
 
 //ex4
+function ageCalculator (birthYear, currentYear) {
+    let age = (currentYear) - (birthYear);
+
+    return `You are ${age} or ${age-1}`
+}
+console.log(ageCalculator(1988, 2022));
+
+//ex5
+
+
+function ageCalculator (birthYear) {
+     let currentYear = new Date().getFullYear();
+console.log(currentYear);
+    let age = (currentYear) - (birthYear);
+
+
+    return `You are ${age} or ${age-1}`
+}
+console.log(ageCalculator(1988));
+
+//ex6
+
+function convertDegToRad (degrees){
+
+    let calcRadians = degrees*(Math.PI/180);
+    return `${degrees} degrees ist equal to ${calcRadians} radians`
+}
+console.log(convertDegToRad(90));
+
+//ex7
+
+function boxSize (width, heihgt, depth){
+    let area = width*heihgt ;
+    let volume = width*heihgt*depth ;
+
+    return ` The area of the box is : ${area} 
+    The volume of the box is : ${volume}` ;
+}
+console.log(boxSize (5,5,5));
